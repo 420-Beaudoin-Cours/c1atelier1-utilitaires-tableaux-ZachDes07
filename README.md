5193ca4565b88f8429ef6cdcd7285f6736e07ff4
420-Beaudoin <55718278+420-Beaudoin@users.noreply.github.com>
2020-01-23 15:28:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# C1A1
+Compléter l'utilitaire Tableaux.java avec les méthodes suivantes:
+
+trierDecroissantSSS reçoit un tableau d'entiers et le tri en ordre décroissant d'entiers (tri SSS).
+
+print reçoit un tableau d'entiers et l'affiche à la console.
+
+maximum reçoit un tableau d'entiers et retourne l'entier maximum.
+
+minimum reçoit un tableau d'entiers et retourne l'entier minimum.
+
+moyenne reçoit un tableau d'entiers et retourne la moyenne (réel).
+
+fouilleSeq reçoit un tableau d'entiers et un élément de type entier recherché et retourne l'indice de l'élément trouvé ou -1.
+
+trierDecroissantSSS reçoit un tableau de chaînes et le tri en ordre décroissant d'entiers (tri SSS).
+
+print reçoit un tableau de chaînes et l'affiche à la console.
+
+maximum reçoit un tableau de chaînes et retourne l'entier maximum.
+
+minimum reçoit un tableau de chaînes et retourne l'entier minimum.
+
+moyenne reçoit un tableau de chaînes et retourne la moyenne (réel).
+
+fouilleSeq reçoit un tableau de chaînes et un élément de type chaîne recherché et retourne l'indice de l'élément trouvé ou -1.
+
+Tester les algorithmes avec la classe Main
---
+++ Tableaux.java
@@ -0,0 +1,42 @@
+/**
+ * @author France Beaudoin
+ */
+public class Tableaux {
+    private static void permute(int[] tab, int i1, int i2){
+        int transit = tab[i1];
+        tab[i1] = tab[i2];
+        tab[i2] = transit;
+    }
+
+    private static void permute(String[] tab, int i1, int i2){
+        String transit = tab[i1];
+        tab[i1] = tab[i2];
+        tab[i2] = transit;
+    }
+
+    public static void trierCroissantSSS(int[] tab){
+        int imin;
+        for (int i=0; i<tab.length-1; i++){
+            imin = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (tab[j] < tab[imin])
+                    imin = j;
+            }
+            if (imin != i)
+                permute(tab, i, imin);
+        }
+    }
+
+    public static void trierCroissantSSS(String[] tab){
+        int imin;
+        for (int i=0; i<tab.length-1; i++){
+            imin = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (Integer.parseInt(tab[j]) < Integer.parseInt(tab[imin]))
+                    imin = j;
+            }
+            if (imin != i)
+                permute(tab, i, imin);
+        }
+    }
+}
